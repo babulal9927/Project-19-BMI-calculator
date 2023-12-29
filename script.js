86569e689a268a6b4e22a81213668ee85e9d2040
babulal9927 <129493914+babulal9927@users.noreply.github.com>
2023-12-29 20:13:27 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+const form = document.querySelector('.form').addEventListener('submit',(e)=>{
+    e.preventDefault();
+
+const height = parseInt(document.querySelector('#height').value);
+const weight = parseInt(document.querySelector('#weight').value);
+const result = document.querySelector('.result');
+
+
+if(height=== '' || height<0 || isNaN(height)){
+    result.innerHTML = 'Please enter a valid input';
+}else if(weight === '' || weight<0 || isNaN(weight)){
+    result.innerHTML = 'Please enter a valid weight'
+}else{
+    const yourBMI = (weight/( (height * height) / 10000)).toFixed(2);
+    result.innerHTML = `<span>${yourBMI}</span>`;
+}
+
+})
